bd1a16b09c094c89eb23db400d237d4d7dc34952
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2018-10-23 17:11:05 -0300

    Atualizando entidade Note

--- Projeto2/src/com/tecweb/entidades/Note.java
+++ Projeto2/src/com/tecweb/entidades/Note.java
@@ -1,6 +1,6 @@
 package com.tecweb.entidades;
 
-import java.util.Date;
+import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,16 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Component
 @Table(name = "Note")
 
-public class Notas {
+public class Note {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id_notes;
+	private Long id_note;
 	
 	@Column(name = "message_body", nullable = true)
 	private String message_body;
@@ -37,12 +36,12 @@ public class Notas {
 	@Column(name = "id_owner", nullable = false)
 	private int id_owner;
 	
-	public Notas() {
+	public Note() {
 		
 	}
-	public Notas(Long id_notes, String message_body, Date creation_date, Date expiry_date, int priority_level, boolean is_active,int id_owner) {
+	public Note(Long id_note, String message_body, Date creation_date, Date expiry_date, int priority_level, boolean is_active,int id_owner) {
 		
-		this.id_notes = id_notes;
+		this.id_note = id_note;
 		this.message_body = message_body;
 		this.creation_date = creation_date;
 		this.expiry_date = expiry_date;
@@ -51,11 +50,11 @@ public class Notas {
 		this.id_owner = id_owner;
 	}
 	
-	public Long getId_notes() {
-		return id_notes;
+	public Long getId_note() {
+		return id_note;
 	}
-	public void setId_notes(Long id_notes) {
-		this.id_notes = id_notes;
+	public void setId_note(Long id_note) {
+		this.id_note = id_note;
 	}
 	
 	public String getMessage_body() {
@@ -65,7 +64,7 @@ public class Notas {
 		this.message_body = message_body;
 	}
 	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	
 	public Date getCreation_date() {
 		return creation_date;
 	}
@@ -73,7 +72,6 @@ public class Notas {
 		this.creation_date = creation_date;
 	}
 	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
 	public Date getExpiry_date() {
 		return expiry_date;
 	}
@@ -88,7 +86,7 @@ public class Notas {
 		this.priority_level = priority_level;
 	}
 	
-	public boolean isIs_active() {
+	public boolean getIs_active() {
 		return is_active;
 	}
 	public void setIs_active(boolean is_active) {
